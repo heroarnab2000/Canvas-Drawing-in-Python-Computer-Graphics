ddb2f08de1a2b4116d4b459ad461188cfb319620
Arnab Das <64526349+heroarnab2000@users.noreply.github.com>
2021-12-16 13:17:50 +0530

    Add files via upload

---
+++ canvas/BoundaryFill.py
@@ -0,0 +1,17 @@
+from graphics import *
+
+def SetPixel(win, x, y, color, pixels):
+    pt = Point(x, y)
+    pixels[x][y] = 2
+    pt.setOutline(color)
+    pt.draw(win)
+
+def BoundFill(win, x, y, color, pixels):
+    if pixels[x][y] != 1 and pixels[x][y] != 2:
+        SetPixel(win, x, y, color, pixels)
+        BoundFill(win, x+1, y, color, pixels)
+        BoundFill(win, x-1, y, color, pixels)
+        BoundFill(win, x, y-1, color, pixels)
+        BoundFill(win, x, y+1, color, pixels)
+
+    return
